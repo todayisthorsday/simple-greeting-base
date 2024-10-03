345129d7d99b81c68f1eae766686d69949975391
Matthew English <matt.english4@gmail.com>
2024-10-03 07:56:21 -0230

    Use better methods of serving static files > > - Use the `express.static` method to serve static files instead of doing it manually

--- index.js
+++ index.js
@@ -5,17 +5,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 
 app.get('/', (request, response) => {
     
-});
-
-app.get('/public/styles.css', (request, response, callback) => {
-    const fileOptions = {
-        root: __dirname,
-        dotfiles: 'deny',
-    };
-    response.sendFile("/public/styles.css", fileOptions);
 });
 
 // Start the server
